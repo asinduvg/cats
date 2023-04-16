8ec55b0fc69240f96772c3a5ba4679b35dc4a091
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-16 13:12:28 +0530

    Complete Traversing, Part 2

--- src/main/scala/part4typeclasses/Traversing.scala
+++ src/main/scala/part4typeclasses/Traversing.scala
@@ -1,6 +1,6 @@
 package part4typeclasses
 
-import cats.{Applicative, Monad}
+import cats.{Applicative, Foldable, Functor, Monad}
 
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
@@ -32,8 +32,8 @@ object Traversing {
 
   // TODO 1
 
-  import cats.syntax.applicative._ // pure
-  import cats.syntax.flatMap._ // flatMap
+  import cats.syntax.applicative._
+  import cats.syntax.flatMap._
   import cats.syntax.functor._ // map
 
   def listTraverse[F[_] : Monad, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
@@ -57,16 +57,74 @@ object Traversing {
   def listSequence[F[_] : Applicative, A](list: List[F[A]]): F[List[A]] =
     listTraverseApplicative(list)(identity)
 
-  // TODO 3
+  // TODO 3 - what's the result of
 
   import cats.instances.vector._
 
   val allpairs = listSequence(List(Vector(1, 2), Vector(3, 4))) // Vector[List[Int]] - all the possible 2-pairs
   val allTriples = listSequence(List(Vector(1, 2), Vector(3, 4), Vector(5, 6))) // Vector[List[Int]] - all the possible 3-pairs
 
+  import cats.instances.option._
+
+  def filterAsOption(list: List[Int])(predicate: Int => Boolean): Option[List[Int]] =
+    listTraverseApplicative[Option, Int, Int](list)(n => Some(n).filter(predicate))
+
+  // TODO 4 - what's the result of
+  val allTrue = filterAsOption(List(2, 4, 6))(_ % 2 == 0) // Some(List(2, 4, 6)
+  val someFalse = filterAsOption(List(1, 2, 3))(_ % 2 == 0) // None
+
+  import cats.data.Validated
+  import cats.instances.list._ // Semigroup[List] => Applicative[ErrorsOr]
+
+  type ErrorsOr[T] = Validated[List[String], T]
+
+  def filterAsValidated(list: List[Int])(predicate: Int => Boolean): ErrorsOr[List[Int]] =
+    listTraverseApplicative[ErrorsOr, Int, Int](list) { n =>
+      if (predicate(n)) Validated.valid(n)
+      else Validated.invalid(List(s"predicate for $n failed"))
+    }
+
+  // TODO 5 - what's the result of
+  val allTrueValidated = filterAsValidated(List(2, 4, 6))(_ % 2 == 0) // Valid(List(2, 4, 6)
+  val someFalseValidated = filterAsValidated(List(1, 2, 3))(_ % 2 == 0) // Invalid(List("predicate for 1 failed", "predicate for 3 failed")
+
+  trait MyTraverse[L[_]] extends Foldable[L] with Functor[L] {
+    def traverse[F[_] : Applicative, A, B](container: L[A])(func: A => F[B]): F[L[B]]
+
+    def sequence[F[_] : Applicative, A](container: L[F[A]]): F[L[A]] =
+      traverse(container)(identity)
+
+    // TODO 6
+    // hint
+    //    type Identity[T] = T
+
+    import cats.Id
+
+    def map[A, B](wa: L[A])(f: A => B): L[B] =
+    //      traverse(wa)(a => f(a).pure[Identity]) or
+    //    traverse[Identity, A, B](wa)(f) or
+      traverse[Id, A, B](wa)(f)
+
+    // If Functor is implemented, map method can be implemented naturally
+  }
+
+  import cats.Traverse
+  import cats.instances.future._ // Applicative[Future]
+
+  val allBandwidthsCats = Traverse[List].traverse(servers)(getBandwidth)
+
+  // extension methods
+
+  import cats.syntax.traverse._ // sequence + traverse
+
+  val allBandwidthsCats2 = servers.traverse(getBandwidth)
+
   def main(args: Array[String]): Unit = {
     println(allpairs)
     println(allTriples)
+    println(allTrue)
+    println(someFalse)
+    println(allTrueValidated)
+    println(someFalseValidated)
   }
-
 }
