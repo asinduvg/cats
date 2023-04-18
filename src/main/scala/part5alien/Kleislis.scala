4c76f1b29f738568a36ab05fadc46da2b3417014
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-18 08:09:54 +0530

    Complete Kleisli

---
+++ src/main/scala/part5alien/Kleislis.scala
@@ -0,0 +1,55 @@
+package part5alien
+
+import cats.data.Reader
+
+object Kleislis {
+
+  val func1: Int => Option[String] = x => if (x % 2 == 0) Some(s"$x is even") else None
+  val func2: Int => Option[Int] = x => Some(x * 3)
+
+  // val func3 = func2 andThen func1
+
+  val plainFunc1: Int => String = x => if (x % 2 == 0) s"$x is even" else "fail"
+  val plainFunc2: Int => Int = x => x * 3
+  val plainFunc3: Int => String = plainFunc2 andThen plainFunc1
+
+  import cats.data.Kleisli
+  import cats.instances.option._ // FlatMap[Option]
+
+  val func1K: Kleisli[Option, Int, String] = Kleisli(func1)
+  val func2K: Kleisli[Option, Int, Int] = Kleisli(func2)
+  val func3K: Kleisli[Option, Int, String] = func2K andThen func1K
+
+  // convenience
+  val multiply = func2K.map(_ * 2) // x => Option(...).map(_ * 2)
+  val chain = func2K.flatMap(x => func1K)
+
+  // TODO
+
+  import cats.Id
+
+  type InterstingKleisli[A, B] = Kleisli[Id, A, B] // wrapper over A => Id[B]
+  // InterstingKleisli = Reader!
+  // hint
+  val times2 = Kleisli[Id, Int, Int](x => x * 2)
+  val plus4 = Kleisli[Id, Int, Int](y => y + 4)
+  val composed = times2.flatMap(t2 => plus4.map(p4 => t2 + p4))
+  val composedFor = for {
+    t2 <- times2
+    p4 <- plus4
+  } yield t2 + p4
+
+  val times2Reader = Reader[Int, Int](x => x * 2)
+  val plus4Reader = Reader[Int, Int](y => y + 4)
+  val composedReader = times2.flatMap(t2 => plus4.map(p4 => t2 + p4))
+  val composedForReader = for {
+    t2 <- times2
+    p4 <- plus4
+  } yield t2 + p4
+
+  def main(args: Array[String]): Unit = {
+    println(composedFor(3)) // 13
+    println(composedForReader(3)) // 13
+  }
+
+}
